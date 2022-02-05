ca8fb1c6e36f84b4fae8252de194df479e6cef05
aaron <aaronmlucia@gmail.com>
2022-02-05 00:01:16 -0500

    36,37 done

--- 37_sum_up_diagonals/sum_up_diagonals.py
+++ 37_sum_up_diagonals/sum_up_diagonals.py
@@ -18,3 +18,25 @@ def sum_up_diagonals(matrix):
         >>> sum_up_diagonals(m2)
         30
     """
+    to_add = []
+    length = len(matrix)
+
+    count = 0
+    while count < length:
+        tup = (count, count)
+        to_add.append(tup)
+        count += 1
+
+    count_y = length - 1
+    count = 0
+    while count_y >= 0:
+        tup = (count_y, count)
+        to_add.append(tup)
+        count_y -= 1
+        count += 1
+
+    sum = 0
+    for tup in to_add:
+        sum += matrix[tup[0]][tup[1]]
+
+    return sum
