f754770925011b6726ee3f148e6e665bfe77b898
aaron <aaronmlucia@gmail.com>
2022-02-08 13:05:45 -0500

    fixed issue with first draft of 30_repeat

--- 30_repeat/repeat.py
+++ 30_repeat/repeat.py
@@ -18,20 +18,22 @@ def repeat(phrase, num):
         >>> repeat('abc', 'nope') is None
         True
     """
-    # res = '' ask ryan why this didn't work
-    # count = num
-    # if isinstance(phrase, str) and isinstance(num, int):
-    #     while count > 0:
-    #         res = res + phrase
-    #         count -= 1
-    # else:
-    #     return None
+    res = ''
+    count = num
+    if isinstance(phrase, str) and isinstance(num, int):
+        while count > 0:
+            res = res + phrase
+            count -= 1
+            print(res, count)
+        return res
+    else:
+        return None
 
     #solution reminded that strings can be multiplied
 
-    if not isinstance(phrase, str) or not isinstance(num, int):
-        return None
-    elif num < 0:
-        return None
-    else:
-        return phrase * num
+    # if not isinstance(phrase, str) or not isinstance(num, int):
+    #     return None
+    # elif num < 0:
+    #     return None
+    # else:
+    #     return phrase * num
